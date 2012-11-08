9148da4fdc46ea185b5693208873145e06b96735
Nuno Job <nunojobpinto@gmail.com>
2012-11-08 04:37:32 +0000

    [fix] spaces for tabs, i mean, wtf

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 var config = require('./config/general');
 var email_service = require ('./lib/notifications/email/email');
 var storage_factory = require ('./lib/storage/storage_factory');
-var services = require ('./lib/services').load_services();
+var services = require ('./lib/dynamic-services').load_services;
 
 var WatchMen = require ('./lib/watchmen');
 var storage = storage_factory.get_storage_instance();
@@ -12,69 +12,56 @@ var watchmen = new WatchMen(services, storage);
 //----------------------------------------------------
 watchmen.on('service_error', function(service, state){
 
-	/*
-	//Do here any additional stuff when you get an error
-	*/
-	var info = service.url_info + ' down!. Error: ' + state.error + '. Retrying in ' +
-			(parseInt(state.next_attempt_secs, 10) / 60) + ' minute(s)..';
+  /*
+  //Do here any additional stuff when you get an error
+  */
+  var info = service.url_info + ' down!. Error: ' + state.error + '. Retrying in ' +
+      (parseInt(state.next_attempt_secs, 10) / 60) + ' minute(s)..';
 
-	console.log (info);
+  console.log (info);
 
-	if (state.prev_state.status === 'success' && config.notifications.enabled){
-		email_service.sendEmail(
-				service.alert_to,
-				service.url_info + ' is down!',
-				service.url_info + ' is down!. Reason: ' + state.error
-		);
-	}
+  if (state.prev_state.status === 'success' && config.notifications.enabled){
+    email_service.sendEmail(
+        service.alert_to,
+        service.url_info + ' is down!',
+        service.url_info + ' is down!. Reason: ' + state.error
+    );
+  }
 });
 
 watchmen.on('service_warning', function(service, state){
 
-	/*
-	//Do here any additional stuff when you get a warning
+  /*
+  //Do here any additional stuff when you get a warning
 
-	console.log (service.url_info + ' WARNING (' + state.elapsed_time + ' ms, avg: '
-			+ state.avg_response_time + ') ## ' + state.warnings + ' warnings');
-	*/
+  console.log (service.url_info + ' WARNING (' + state.elapsed_time + ' ms, avg: '
+      + state.avg_response_time + ') ## ' + state.warnings + ' warnings');
+  */
 
 });
 
 watchmen.on('service_back', function(service, state){
-	if (config.notifications.enabled){
-		email_service.sendEmail(
-				service.alert_to,
-				service.url_info + ' is back!',
-				service.url_info + ' ' +  service.msg
-		);
-	}
+  if (config.notifications.enabled){
+    email_service.sendEmail(
+        service.alert_to,
+        service.url_info + ' is back!',
+        service.url_info + ' ' +  service.msg
+    );
+  }
 });
 
 watchmen.on('service_ok', function(service, state){
-	/*
-	//Do here any additional stuff when you get a successful response
+  /*
+  //Do here any additional stuff when you get a successful response
 
-	console.log (service.url_info + ' responded OK! (' + state.elapsed_time + ' milliseconds, avg: '
-			+ state.avg_response_time + ')');
+  console.log (service.url_info + ' responded OK! (' + state.elapsed_time + ' milliseconds, avg: '
+      + state.avg_response_time + ')');
   */
 });
 
 //----------------------------------------------------
 // Start watchmen
 //----------------------------------------------------
-watchmen.start();
-
-
-//----------------------------------------------------
-// Error handling
-//----------------------------------------------------
-process.on('uncaughtException', function(err) {
-	console.error('uncaughtException:');
-	console.error(err);
-});
-
-process.on('SIGINT', function () {
-	console.log('stopping watchmen..');
-	storage.quit();
-	process.exit(0);
+watchmen.start(function () {
+  console.log("started");
 });
